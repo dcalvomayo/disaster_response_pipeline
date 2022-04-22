f0636844ba49a87fabe34fed7dd7e842dc331354
Daniel <dcalvomayo@gmail.com>
2022-04-22 21:14:52 +0200

    Reset Gridsearch parameters

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -94,15 +94,14 @@ def build_model():
                 ])
 
     parameters = {
-            'vect__max_df': (0.5,0.75,1.0),
-            'vect__ngram_range': ((1,1), (1,2)),
-            'vect__max_features': (None, 5000, 10000),
-            'tfidf__use_idf': (True, False),
-            'clf__n_estimators': [50, 100, 200],
-            'clf__min_samples_split': [2, 3, 4],
+            'vect__max_df': (0.2, 1.0),
+            'tfidf__norm': ('l1','l2'),
+            'clf__estimator__n_estimators': [10, 100],
+            'clf__estimator__min_samples_split': [2, 5],
             }
 
-    cv = GridSearchCV(pipeline, param_grid=parameters)
+    # Remove verbose parameter to reduce output
+    cv = GridSearchCV(pipeline, param_grid=parameters, verbose=10)
 
     return cv
 

b33a4c52777d4da57dbb3fccd6b95680e250f1d2
Daniel <dcalvomayo@gmail.com>
2022-04-23 11:52:12 +0200

    Added parameter to .to_sql to avoid failure when table already exists

--- data/process_data.py
+++ data/process_data.py
@@ -81,7 +81,7 @@ def save_data(df, database_filename):
     """
 
     engine = create_engine('sqlite:///' + database_filename)
-    df.to_sql('DisasterData', engine, index=False)  
+    df.to_sql('DisasterData', engine, index=False, if_exist='replace')  
 
 def main():
     if len(sys.argv) == 4:
